96be7bd9788f97862303dd442678c067bab995e4
lukespowerconverters <rachsolak@gmail.com>
2022-12-13 14:25:20 -0500

    parse data for station names, locations, and correct side for doors to open

--- TrackModel/extra.py
+++ TrackModel/extra.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 #class track_info:
 lines = []
+
 db_conn = sqlite3.connect("ece1140/TrackModel/data/trackmodel.db")
 c = db_conn.cursor()
 
@@ -26,7 +27,7 @@ def read_new_data():
         table.to_sql(table_name, db_conn, if_exists='replace', index=False)
 
     ## Create a new train table
-    new_train_table()
+    #new_train_table()
 
 def new_train_table():
     ## Create new table in the SQLite database
@@ -40,4 +41,36 @@ def new_train_table():
         """
     )
 
+def new_station_list():
+    stations = []
+
+    c.execute(
+        """
+        SELECT
+            Line,
+            "Block Number",
+            Infrastructure,
+            "Station Side"
+        FROM 
+            'Green Line';
+        """
+    )
+    infra_info = c.fetchall()
+    for infra in infra_info:
+        #print(infra[2])
+        str_infra = str(infra[2]).upper()
+        found = str_infra.find("STATION")
+        if found != -1:
+            name = str_infra[(found + 9):]
+
+            ## Get rid of switch info, just want station name
+            semi = name.find(";")
+            if semi != -1:
+                name = name[:semi]
+
+            ## format
+            name = name.strip()
+            print(name, " ", str(infra[3]))
+
 read_new_data()
+new_station_list()
